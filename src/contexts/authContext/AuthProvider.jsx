f12fec51ee9c763aa0f4a4884b40b27ae4bcae9f
RaiyanSohel-byte <afnan19262@gmail.com>
2025-11-08 22:32:19 +0600

    firebase config added

---
+++ src/contexts/authContext/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase.config";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const authInfo = {
+    loading,
+    setLoading,
+    user,
+    setUser,
+    createUser,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
